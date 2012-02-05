e35cbd1af45e964798f8317d0f9d91ba411c02da
Melodi Cowan <melodi.cowan@gmail.com>
2012-02-04 18:18:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabindrag::Application.config.secret_token = '10d880126912e6d71fcf9564f57f4f5b8f1ed2e643991ba92eba64a951b0873e41e6ffe20eea4b12788c7d75200a6d8050ce71aa7935eb81a6dca165f57484d1'
